867f8dfacfa99560070d91cf299396fe5d9511e7
knhbae <knhbae@gmail.com>
2020-01-23 01:03:46 +0900

    connect to DB server

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,29 @@ import "./App.css";
 import ObjectItem from "./components/OjbectItem";
 import UserEmotion from "./components/UserEmotion";
 
+//Progress Bar
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    minWidth: 1080
+    // marginTop: theme.spacing(3),
+    // overflowX: "auto"
+  },
+  // table: {
+  //   minWidth: 1080
+  // },
+  progress: {
+    marginTop: theme.spacing(2)
+  }
+});
+
 class App extends Component {
   state = {
-    objectItems: ""
+    objectItems: "",
+    completed: 0
   };
 
   componentDidMount() {
@@ -20,19 +40,28 @@ class App extends Component {
     const body = await response.json();
     return body;
   };
-
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
   render() {
+    const { classes } = this.props;
     return (
       <div>
-        {this.state.objectItems
-          ? this.state.objectItems.map(c => {
-              return <ObjectItem key={c.id} id={c.id} item={c.item} />;
-            })
-          : ""}
+        {this.state.objectItems ? (
+          this.state.objectItems.map(c => {
+            return <ObjectItem key={c.id} id={c.id} item={c.item} />;
+          })
+        ) : (
+          <CircularProgress
+            className={classes.progress}
+            variant="determinate"
+            value={this.state.completed}
+          />
+        )}
         <UserEmotion />
       </div>
     );
   }
 }
-
-export default App;
+export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import express from "express";
 // import { json, urlencoded } from "body-parser";
+const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -8,17 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync("./database.json");
+const conf = JSON.parse(data);
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 app.get("/api/objectItems", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      item: "#출근"
-    },
-    {
-      id: 2,
-      item: "#퇴근"
-    }
-  ]);
+  connection.query("select * from object_item", (err, rows, fields) => {
+    res.send(rows);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
